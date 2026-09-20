ce3fb9d72c8d11a35e2824174311775e9df45c26
WIN-QUG5H71A7UD <109420678+OlexaGryschenko@users.noreply.github.com>
2026-09-20 19:12:01 +0100

    fix: add http-error

--- src/server.js
+++ src/server.js
@@ -16,8 +16,8 @@ import notesRouter from './routes/notesRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-console.log("PORT from env:", process.env.PORT); // Має вивести значення твого PORT
-console.log("DB_URI from env:", process.env.MONGODB_URI); // Має вивести твою URI базу даних
+/* console.log("PORT from env:", process.env.PORT); // Має вивести значення твого PORT
+console.log("DB_URI from env:", process.env.MONGO_URl); // Має вивести твою URI базу даних */
 
 /* Middleware */
 app.use(logger);
